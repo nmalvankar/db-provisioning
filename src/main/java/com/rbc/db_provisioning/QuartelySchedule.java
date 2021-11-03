4bb6d622a4d77f89cec943e59ca2dccffdc1fcc7
adminUser <>
2021-11-03 13:53:27 +0000

    {/src/main/java/com/rbc/db_provisioning/QuartelySchedule.java}

--- src/main/java/com/rbc/db_provisioning/QuartelySchedule.java
+++ src/main/java/com/rbc/db_provisioning/QuartelySchedule.java
@@ -6,12 +6,44 @@ package com.rbc.db_provisioning;
 
 public class QuartelySchedule implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public QuartelySchedule() {
-    }
+	private boolean enabled;
+	private java.lang.String startMonth;
+	private int dayOfMonth;
 
+	public QuartelySchedule() {
+	}
 
+	public boolean isEnabled() {
+		return this.enabled;
+	}
 
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public java.lang.String getStartMonth() {
+		return this.startMonth;
+	}
+
+	public void setStartMonth(java.lang.String startMonth) {
+		this.startMonth = startMonth;
+	}
+
+	public int getDayOfMonth() {
+		return this.dayOfMonth;
+	}
+
+	public void setDayOfMonth(int dayOfMonth) {
+		this.dayOfMonth = dayOfMonth;
+	}
+
+	public QuartelySchedule(boolean enabled, java.lang.String startMonth,
+			int dayOfMonth) {
+		this.enabled = enabled;
+		this.startMonth = startMonth;
+		this.dayOfMonth = dayOfMonth;
+	}
 
 }
