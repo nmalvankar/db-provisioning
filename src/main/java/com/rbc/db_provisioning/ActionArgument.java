c7ccb73f6c2369e4174f9ce84d16f3270f3a50fd
adminUser <>
2021-11-03 14:08:14 +0000

    {/src/main/java/com/rbc/db_provisioning/ActionArgument.java}

--- src/main/java/com/rbc/db_provisioning/ActionArgument.java
+++ src/main/java/com/rbc/db_provisioning/ActionArgument.java
@@ -6,12 +6,33 @@ package com.rbc.db_provisioning;
 
 public class ActionArgument implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public ActionArgument() {
-    }
+	private java.lang.String name;
+	private java.lang.String value;
 
+	public ActionArgument() {
+	}
 
+	public java.lang.String getName() {
+		return this.name;
+	}
 
+	public void setName(java.lang.String name) {
+		this.name = name;
+	}
+
+	public java.lang.String getValue() {
+		return this.value;
+	}
+
+	public void setValue(java.lang.String value) {
+		this.value = value;
+	}
+
+	public ActionArgument(java.lang.String name, java.lang.String value) {
+		this.name = name;
+		this.value = value;
+	}
 
 }
